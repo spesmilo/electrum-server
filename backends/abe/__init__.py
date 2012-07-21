f331a62d89f3f09f0834514e5c5e67306495c706
coblee <chocobo@alum.mit.edu>
2012-07-20 22:50:42 -0700

    Fix get_block_number bug.

--- backends/abe/__init__.py
+++ backends/abe/__init__.py
@@ -424,7 +424,7 @@ class AbeStore(Datastore_class):
         with store.dblock:
             store.catch_up()
             store.memorypool_update()
-            block_number = store.get_block_number(self.chain_id)
+            block_number = store.get_block_number(store.chain_id)
             return block_number
 
 
