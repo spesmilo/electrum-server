6a3dcc17d69dfb6de16db254599d5d92ed016588
Ben Reeves <zootreeves@gmail.com>
2012-10-10 17:40:29 +0100

    Fix indentation

--- transports/stratum_http.py
+++ transports/stratum_http.py
@@ -136,12 +136,12 @@ class StratumJSONRPCRequestHandler(
         SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
             
     def do_OPTIONS(self):
-            self.send_response(200)
-	        self.send_header('Allow', 'GET, POST, OPTIONS')
-	        self.send_header('Access-Control-Allow-Origin', '*')
-	        self.send_header('Access-Control-Allow-Headers', 'X-Request, X-Requested-With')
-	        self.send_header('Content-Length', '0')
-	        self.end_headers()
+        self.send_response(200)
+        self.send_header('Allow', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Headers', 'X-Request, X-Requested-With')
+        self.send_header('Content-Length', '0')
+        self.end_headers()
             
     def do_GET(self):
         if not self.is_rpc_path_valid():
