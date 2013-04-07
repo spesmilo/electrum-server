7527c8563c61630353d34c98002245c6b3dcd035
ThomasV <thomasv@gitorious>
2013-04-07 07:46:54 +0400

    fix

--- backends/bitcoind/blockchain_processor.py
+++ backends/bitcoind/blockchain_processor.py
@@ -68,6 +68,7 @@ class BlockchainProcessor(Processor):
             print_log('initializing database')
             self.height = 0
             self.last_hash = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+            db_version = self.db_version
 
         # check version
         if self.db_version != db_version:
